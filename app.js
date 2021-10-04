32faea49529dc394fc1930ac4919c996e01dadd7
Madison Sanchez-Forman <msanchezforman@pugetsound.edu>
2021-10-03 19:00:35 -0700

    get_root_words added

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("testing");
+var min_len = 3, max_len = 6,
+guessedWords = [],
+hiddenWords = [],
+availableWords = [],
+rootWords = [];
+generate_dictionary();
+get_root_words();
+
+function generate_dictionary(){
+    for(index = 0; index < dictionary.length; index++){
+        if(dictionary[index].length >= min_len && dictionary[index].length <= max_len) {
+            availableWords.push(dictionary[index]);
+        }
+    }
+}
+function get_root_words(){
+    for(index = 0; index < availableWords.length; index++)
+    if(availableWords[index].length == max_len) {
+        rootWords.push(availableWords[index]);
+    }
+}
+
+let startWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+console.log(startWord);
